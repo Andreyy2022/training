e11ac9c9d08367ee3103fe45e82bc2a4cd47f540
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-04-27 19:57:37 +0300

    training2

--- asynchrony.js
+++ asynchrony.js
@@ -1,68 +1,38 @@
 'use strict';
 
-const UL = document.querySelector('#list');
-const amountP = document.querySelector('#amount');
-const resultP = document.querySelector('#result');
+let startTime = new Date();
+let startHour = startTime.getHours();
+let startMinutes = startTime.getMinutes();
+let startSeconds = startTime.getSeconds();
 
-function mess(arr) {
-    alert('we got the arr ['+ arr +']' + ' !');
+let diffSec = 60 - startSeconds;
+
+let nextMinute = {
+    0: ' 0 minutes for now',
+    1: ' 1 minutes for now',
+    2: ' 2 minutes for now',
+    3: ' 3 minutes for now',
+    4: ' 4 minutes for now',
+    5: ' 5 minutes for now',
+    6: ' 6 minutes for now',
+    7: ' 7 minutes for now',
+    8: ' 8 minutes for now',
+    9: ' 9 minutes for now',
+    10: ' 10 minutes for now',
+    11: ' 11 minutes for now',
+    12: ' 12 minutes for now',
+    13: ' 13 minutes for now',
+    14: ' 14 minutes for now',
+
+
 }
-
-function list(arr) {
-    for (let elem of arr) {
-        let LI = document.createElement('li');
-        LI.innerHTML = elem;
-        UL.append(LI);
-    }
-}
-
-function amount(arr) {
-    let count = 0;
-    for (let elem of arr) {
-        ++count;
-    }
-
-    amountP.innerHTML = count;
-}
-
-function resSum(arr) {
-    let res = 0;
-    for (let elem of arr) {
-        res += elem;
-    }
-
-    resultP.innerHTML = res;
-}
-
-
-let subscribers = {
-    'mess': [mess],
-    'list': [list],
-    'amountSum': [amount, resSum]
-}
-
-function on(name, callback) {
-    if (!subscribers[name]) {
-        subscribers[name] = [];
-    }
-
-    subscribers[name].push(callback);
-}
-
-function emit(name, data) {
-    if (subscribers[name]) {
-        for (let callback of subscribers[name]) {
-            callback(data);
-        }
-    }
-}
-
-function somthing(arr) {
-    console.log(arr);
-}
-
-on('some', somthing);
 
 setTimeout( function() {
-    emit('some', [1, 2, 3, 4])
-}, 3000);
+    console.log(new Date().getMinutes() + ' minutes for now');
+
+        setTimeout( function() {
+            console.log(new Date().getMinutes() + ' minutes for now (.)(.)');
+        }, 60 * 1000 );
+
+}, diffSec * 1000 );
+
