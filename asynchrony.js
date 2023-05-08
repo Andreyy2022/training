12ac12da47334491ab1bc2ff33ad5e49d334ba91
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-05-08 12:15:39 +0300

    promAgain

--- asynchrony.js
+++ asynchrony.js
@@ -1,28 +1,18 @@
 'use strict';
 
-function randoms() {
-    return Math.floor(Math.random() * 11);
-}
-
-function complex() {
-    return new  Promise( resolve => setTimeout(() => resolve(randoms()), randoms() * 1000 ) );
-}
-
-let promises = [];
-
-for (let i = 1; i <= 10; i++) {
-    promises.push(complex());
-}
-
-Promise.all(promises).then(
-    function(res) {
-        console.log(res);
-        
-        let sum = 0;
-        for (let elem of res) {
-            sum += elem;
+function fun(num) {
+    if (num > 0) {
+        return new Promise(function(resolve) {
+            setTimeout(function() {
+                resolve(num * num);
+            }, 3000);
         }
-
-        console.log(sum);
+        );
+    } else {
+        return Promise.resolve(0);
     }
-);
+}
+
+fun(0).then(function(res) {
+    console.log(res);
+})
