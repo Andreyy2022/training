7ba7576cc2158dd7318158b6d507821cbed76d18
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-05-09 12:29:38 +0300

    promiseImages

--- asynchrony.js
+++ asynchrony.js
@@ -1,22 +1,55 @@
 'use strict';
 
-function loadImage(path) {
-    return new Promise(function(resolve, reject) {
-        let image = document.createElement('img');
-        image.src = path;
+let paths = ['tree.png', 'crocodile.png', 'Warface_sample.jpg'];
 
-        image.addEventListener('load', function() {
-            resolve(image);
-        });
+function loadImages(paths) {
+    let promises;
+    return promises = [
+        new Promise(function(resolve, reject) {
+            let image1 = document.createElement('img');
+            image1.src = paths[0];
 
-        image.addEventListener('error', function() {
-            reject(new Error('image " ' + path + ' " error'));
-        });
-    });
+            image1.addEventListener('load', function() {
+                resolve(image1);
+            });
+
+            image1.addEventListener('error', function() {
+                reject(new Error('error loading "' + paths[0] + '"'));
+            });
+        }),
+
+        new Promise(function(resolve, reject) {
+            let image2 = document.createElement('img');
+            image2.src = paths[1];
+
+            image2.addEventListener('load', function() {
+                resolve(image2);
+            });
+
+            image2.addEventListener('error', function() {
+                reject(new Error('error loading "' + paths[1] + '"'));
+            });
+        }),
+
+        new Promise(function(resolve, reject) {
+            let image3 = document.createElement('img');
+            image3.src = paths[2];
+
+            image3.addEventListener('load', function() {
+                resolve(image3);
+            });
+
+            image3.addEventListener('error', function() {
+                reject('error loading "' + paths[2] + '"');
+            });
+        })
+    ]
 }
 
-loadImage('./tree.png').then(function(image) {
-    document.body.appendChild(image);
-}).catch(function(error) {
-    console.log(error);
-})
+loadImages(paths).Promise.all(promises).then(function(res) {
+    for (let elem of res) {
+        document.body.appendChild(elem);
+    }
+}).catch(function(err) {
+    console.log(err);
+});
