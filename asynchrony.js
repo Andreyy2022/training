b797498ef990ff3ad3246ef144c63bfb3b686e8c
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-05-10 11:15:14 +0300

    oneMore

--- asynchrony.js
+++ asynchrony.js
@@ -1,53 +1,11 @@
 'use strict';
 
-let paths = ['tree.png', 'crocodile.png', 'Warface_sample.jpg'];
-
-let promises = [
-        new Promise(function(resolve, reject) {
-            let image1 = document.createElement('img');
-            image1.src = paths[0];
-
-            image1.addEventListener('load', function() {
-                resolve(image1);
-            });
-
-            image1.addEventListener('error', function() {
-                reject(new Error('error loading "' + paths[0] + '"'));
-            });
-        }),
-
-        new Promise(function(resolve, reject) {
-            let image2 = document.createElement('img');
-            image2.src = paths[1];
-
-            image2.addEventListener('load', function() {
-                resolve(image2);
-            });
-
-            image2.addEventListener('error', function() {
-                reject(new Error('error loading "' + paths[1] + '"'));
-            });
-        }),
-
-        new Promise(function(resolve, reject) {
-            let image3 = document.createElement('img');
-            image3.src = paths[2];
-
-            image3.addEventListener('load', function() {
-                resolve(image3);
-            });
-
-            image3.addEventListener('error', function() {
-                reject('error loading "' + paths[2] + '"');
-            });
-        })
-    ]
-
-
-Promise.all(promises).then(function(res) {
-    for (let elem of res) {
-        document.body.appendChild(elem);
-    }
-}).catch(function(err) {
-    console.log(err);
+let promise = new Promise(function(resolve, reject) {
+    window.addEventListener('DOMContentLoaded', function() {
+        resolve('DOM loaded');
+    });
 });
+
+promise.then(function(res) {
+    console.log(res);
+})
