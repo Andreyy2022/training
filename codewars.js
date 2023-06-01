47a865e316c8fdc5120b6fffb963105d514e158c
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-06-01 21:02:15 +0300

    CodeMorse2Finished

--- codewars.js
+++ codewars.js
@@ -1,30 +1,73 @@
 'use strict';
 
-let decodeMorse = function(morseCode){
-    // Your code here
-    // You can use MORSE_CODE[morse]
-    
-    let resultWords = [];
-    
-    let arrMorseWords = morseCode.split('   ');
-    for (let MorseLetter of arrMorseWords) {
-      let arrMorseLetters = MorseLetter.split(' ');
-      
-      let resultLetters = [];
-      for (let i = 0; i < arrMorseLetters.length; i++) {
-        if (MORSE_CODE[arrMorseLetters[i]]) {
-          resultLetters.push(arrMorseLetters[i]);
-          
-          if (i == arrMorseLetters.length - 1) {
-            resultLetters.push(' ');
-          }
-        }
-      } 
-      
-      resultWords.push(resultLetters);
-    }
-    
-    return resultWords.join(' ');
-  }
+let MORSE_CODE = { 
+  '.-':     'a',
+  '-...':   'b',
+  '-.-.':   'c',
+  '-..':    'd',
+  '.':      'e',
+  '..-.':   'f',
+  '--.':    'g',
+  '....':   'h',
+  '..':     'i',
+  '.---':   'j',
+  '-.-':    'k',
+  '.-..':   'l',
+  '--':     'm',
+  '-.':     'n',
+  '---':    'o',
+  '.--.':   'p',
+  '--.-':   'q',
+  '.-.':    'r',
+  '...':    's',
+  '-':      't',
+  '..-':    'u',
+  '...-':   'v',
+  '.--':    'w',
+  '-..-':   'x',
+  '-.--':   'y',
+  '--..':   'z',
+  '.----':  '1',
+  '..---':  '2',
+  '...--':  '3',
+  '....-':  '4',
+  '.....':  '5',
+  '-....':  '6',
+  '--...':  '7',
+  '---..':  '8',
+  '----.':  '9',
+  '-----':  '0',
+};
 
-  decodeMorse('.... . -.--   .--- ..- -.. .');
+let decodeMorse = function(morseCode){
+  // Your code here
+  // You can use MORSE_CODE[morse]
+  
+  let resultWords = [];
+  let resultLetters;
+  
+  let arrMorseWords = morseCode.split('   ');
+    
+  for (let MorseWord of arrMorseWords) {
+    let arrMorseWord = MorseWord.split(' ');
+    
+    resultLetters = [];
+    
+    for (let i = 0; i < arrMorseWord.length; i++) {
+      if ( MORSE_CODE[arrMorseWord[i]] ) {
+
+        resultLetters.push( MORSE_CODE[arrMorseWord[i]] );
+      }
+
+    }
+
+    resultLetters = resultLetters.join('');
+    
+    resultWords.push(resultLetters);
+  }
+  
+  resultWords = resultWords.join(' ');
+  return resultWords.trim();
+}
+  
+console.log( decodeMorse('.... . -.--   .--- ..- -.. .') ); 
