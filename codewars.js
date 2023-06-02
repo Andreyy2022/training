2b0bb041380bd339534bed1f67e4fc7fa15076bf
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-06-02 20:18:45 +0300

    oddNum

--- codewars.js
+++ codewars.js
@@ -1,73 +1,16 @@
 'use strict';
 
-let MORSE_CODE = { 
-  '.-':     'a',
-  '-...':   'b',
-  '-.-.':   'c',
-  '-..':    'd',
-  '.':      'e',
-  '..-.':   'f',
-  '--.':    'g',
-  '....':   'h',
-  '..':     'i',
-  '.---':   'j',
-  '-.-':    'k',
-  '.-..':   'l',
-  '--':     'm',
-  '-.':     'n',
-  '---':    'o',
-  '.--.':   'p',
-  '--.-':   'q',
-  '.-.':    'r',
-  '...':    's',
-  '-':      't',
-  '..-':    'u',
-  '...-':   'v',
-  '.--':    'w',
-  '-..-':   'x',
-  '-.--':   'y',
-  '--..':   'z',
-  '.----':  '1',
-  '..---':  '2',
-  '...--':  '3',
-  '....-':  '4',
-  '.....':  '5',
-  '-....':  '6',
-  '--...':  '7',
-  '---..':  '8',
-  '----.':  '9',
-  '-----':  '0',
-};
-
-let decodeMorse = function(morseCode){
-  // Your code here
-  // You can use MORSE_CODE[morse]
-  
-  let resultWords = [];
-  let resultLetters;
-  
-  let arrMorseWords = morseCode.split('   ');
-    
-  for (let MorseWord of arrMorseWords) {
-    let arrMorseWord = MorseWord.split(' ');
-    
-    resultLetters = [];
-    
-    for (let i = 0; i < arrMorseWord.length; i++) {
-      if ( MORSE_CODE[arrMorseWord[i]] ) {
-
-        resultLetters.push( MORSE_CODE[arrMorseWord[i]] );
-      }
-
-    }
-
-    resultLetters = resultLetters.join('');
-    
-    resultWords.push(resultLetters);
-  }
-  
-  resultWords = resultWords.join(' ');
-  return resultWords.trim();
+function findOdd(A) {
+  //happy coding!
+  return 0;
 }
+
+function (arr) {
+  let clon = 0;
   
-console.log( decodeMorse('.... . -.--   .--- ..- -.. .') ); 
+  for (let i = 0; i < arr.length; i++) {
+    if ()
+    
+    clon += arr[i];
+  }
+}
