e1dc5d0a5f9331e4e84d8bcd1ee276bb9009dd0d
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-06-08 20:50:28 +0300

    compSame

--- codewars.js
+++ codewars.js
@@ -1,22 +1,29 @@
 'use strict';
 
-function count(string) {
-  // TODO
-  let arrString = string.split('');
-  
-  return getObjCount(arrString);;
-}
+let a = [121, 144, 19, 161, 19, 144, 19, 11];
+let b = [121, 14641, 20736, 361, 25921, 361, 20736, 361];
 
-function getObjCount(arr) {
-  let objCount = {};
-  
-  for (let elem of arr) {
-    if (objCount[elem] === undefined) {
-      objCount[elem] = 1;
+function comp(array1, array2){
+  //your code here
+  for (let elem of array2) {
+    if ( checkSqElem( sqrtElem(elem), array1 ) !== true ) {
+      return false;
     } else {
-      objCount[elem]++;
+      return true;
     }
   }
-  
-  return objCount;
 }
+
+function sqrtElem(elem) {
+
+  console.log(Math.sqrt(elem));
+  return Math.sqrt(elem);
+}
+
+function checkSqElem(elem, arr) {
+
+  console.log(arr.filter(item => item == elem));
+  return arr.filter(item => item == elem);
+}
+
+console.log(comp(a, b));
