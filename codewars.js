8a689b78e983637c08e246a5ce7a23dae094729f
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-06-09 19:56:59 +0300

    campSameDone

--- codewars.js
+++ codewars.js
@@ -1,17 +1,17 @@
 'use strict';
 
 let a = [121, 144, 19, 161, 19, 144, 19, 11];
-let b = [121, 14641, 20736, 361, 25921, 361, 20736, 361];
+let b = [121, 14641, 20736, 361, 25921, 361, 20736, 362];
 
 function comp(array1, array2){
   //your code here
   for (let elem of array2) {
     if ( checkSqElem( sqrtElem(elem), array1 ) !== true ) {
       return false;
-    } else {
-      return true;
     }
+      
   }
+  return true;
 }
 
 function sqrtElem(elem) {
@@ -22,8 +22,8 @@ function sqrtElem(elem) {
 
 function checkSqElem(elem, arr) {
 
-  console.log(arr.filter(item => item == elem));
-  return arr.filter(item => item == elem);
+  console.log( arr.includes(elem) );
+  return arr.includes(elem);
 }
 
 console.log(comp(a, b));
