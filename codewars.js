e5670242f6d61ba9b55e20affd7333c3659a88d2
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-06-15 00:18:08 +0300

    uniqueNum

--- codewars.js
+++ codewars.js
@@ -1,25 +1,12 @@
 'use strict';
 
-function findMissingLetter(array)
-{
-  let alphabetUp = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-  let alphabetLow = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
-  let partAlph;
-
-  if (alphabetUp.includes(array[0])) {
-    partAlph = alphabetUp.slice( alphabetUp.indexOf(array[0]), alphabetUp.indexOf(array[array.length-1]) + 1 );
-  } else {
-    partAlph = alphabetLow.slice( alphabetLow.indexOf(array[0]), alphabetLow.indexOf(array[(array.length-1)]) + 1 );
-  }
-  
-  for (let elem of partAlph) {
-    if ( !array.includes(elem) ) {
-
-      return elem;  
+function findUniq(arr) {
+  // do magic
+  for (let i  = 0; i < arr.length; i++) {
+    if (arr[i] !== arr[i+1]) {
+      return arr[i+1];
     }
   }
-
 }
 
-console.log( findMissingLetter(['a','b','c','d','f']) );
+console.log(findUniq([ 0, 0, 0.55, 0, 0 ]));
