4f3613914de7eade0bfc0aa7ba934c6a9e540ebd
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-06-17 11:04:01 +0300

    uniqueStr 1.0.2

--- codewars.js
+++ codewars.js
@@ -6,21 +6,28 @@ function findUniq(arr) {
   console.log(ARR);
 
   for (let i = 0; i < ARR.length; i++) {
+
+    console.log(arr[i]);
+      if (ARR[i] === '' && ARR[i + 1] !== '') {
+      return arr[i + 1];
+      }
+    
     for (let j = 0; j < ARR[i].length; j++) {
 
+
+
+
       if (!ARR[i + 1].includes(ARR[i][j])) {
-        let index = ARR.indexOf(ARR[i + 1]);
         
           if (i != ARR.length - 2)
             if (!ARR[i + 2].includes(ARR[i][j])) {
               return arr[0];
             }
         
-        return arr[index];
+        return arr[i + 1];
       }
-
     }
   }
 }
 
-console.log(findUniq([ 'SILVIA', 'VASILI', 'VICTOR' ]));
+console.log(findUniq([ '', '', '', 'A', '', '' ]));
