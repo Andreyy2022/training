17325560ff529a63035bc536c869c450b79d0a2d
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-06-17 12:37:04 +0300

    unique 1.0.1

--- codewars.js
+++ codewars.js
@@ -2,28 +2,20 @@
 
 function findUniq(arr) {
   // do magic
-  let ARR = arr.join().toUpperCase().split(',');
-  console.log(ARR);
+  for (let i = 0; i < arr.length; i++) {
 
-  for (let i = 0; i < ARR.length; i++) {
-
-      if (ARR[i] === '' && ARR[i + 1] !== '') {
+    if (arr[i] === '' && arr[i + 1] !== '') {
       return arr[i + 1];
-      }
-    
-      if (ARR[i] != ARR[i + 1]) {
+    }
+  
+    if (arr[i] !== arr[i + 1]) {
+      return arr[i + 1];
+    }
 
-        if (ARR[i + 2] === undefined) {
-          return arr[arr.length - 1];
-        }
-
-        if (!ARR[i + 2].includes(ARR[i])) {
-          return arr[0];
-        }
-
-        return arr[i + 1];
+    if (arr[i + 2] === undefined) {
+      return arr[arr.length - 1];
       }
   }
 }
 
-console.log(findUniq([ '8', '8', '8', '8', '8', '8', '8', '7' ]));
+console.log(findUniq([ '8', '8', '8', 8, '8', '8', '8', '8' ]));
