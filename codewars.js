3a929f285ec26985623265b6bf4eef937bc5333c
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-06-19 19:54:11 +0300

    unique 1.0.2

--- codewars.js
+++ codewars.js
@@ -12,10 +12,12 @@ function findUniq(arr) {
       return arr[i + 1];
     }
 
-    if (arr[i + 2] === undefined) {
+    if (arr[i + 2] > arr.length) {
       return arr[arr.length - 1];
       }
   }
 }
 
-console.log(findUniq([ '8', '8', '8', 8, '8', '8', '8', '8' ]));
+console.log(findUniq([ '8', '8', '8', 'undefined', '8', '8', '8', '8' ]));
+
+console.log( typeof(findUniq([ '8', '8', '8', 'undefined', '8', '8', '8', '8' ])) );
