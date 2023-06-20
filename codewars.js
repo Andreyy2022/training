720948abd33482872e1626b6ffed05c477383c7b
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-06-20 19:16:35 +0300

    unique 1.0.3

--- codewars.js
+++ codewars.js
@@ -4,6 +4,14 @@ function findUniq(arr) {
   // do magic
   for (let i = 0; i < arr.length; i++) {
 
+    if (isNaN(arr[i])) {
+
+      if (!isNaN(arr[i + 1])) {
+        return arr[i + 1];
+      }
+      
+    } else {
+
     if (arr[i] === '' && arr[i + 1] !== '') {
       return arr[i + 1];
     }
@@ -14,10 +22,11 @@ function findUniq(arr) {
 
     if (arr[i + 2] > arr.length) {
       return arr[arr.length - 1];
-      }
+    }
+    }
   }
 }
 
-console.log(findUniq([ '8', '8', '8', 'undefined', '8', '8', '8', '8' ]));
+console.log(findUniq([ NaN, NaN, NaN, 0.7996041476602642, NaN ]));
 
-console.log( typeof(findUniq([ '8', '8', '8', 'undefined', '8', '8', '8', '8' ])) );
+console.log( typeof(findUniq([ NaN, NaN, NaN, 0.7996041476602642, NaN ])) );
