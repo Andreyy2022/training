4bf2012956fa0519d5f8a7cbe126876c03a94d88
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-06-22 20:13:39 +0300

    calcFunc 1.0.1

--- codewars.js
+++ codewars.js
@@ -1,33 +1,51 @@
 'use strict';
 
-let a;
-
-function zero() {}
-function one() {}
-function two() {}
-function three() {}
-function four() {}
-function five() {
-  a = 5;
-  console.log();
-  return a;
+function zero(func) {
+  return func ? func(0) : 0;
 }
-function six() {}
-function seven() {
-  a = 7;
-  console.log();
-  return a;
+function one(func) {
+  return func ? func(1) : 1;
 }
-function eight() {}
-function nine() {}
-
-function plus() {}
-function minus() {}
-function times() {
-  console.log(a);
-
-  return a * a;
+function two(func) {
+  return func ? func(2) : 2;
 }
-function dividedBy() {}
+function three(func) {
+  return func ? func(3) : 3;
+}
+function four(func) {
+  return func ? func(4) : 4;
+}
+function five(func) {
+  return func ? func(5) : 5;
+}
+function six(func) {
+  return func ? func(6) : 6;
+}
+function seven(func) {
+  return func ? func(7) : 7;
+}
+function eight(func) {
+  return func ? func(8) : 8;
+}
+function nine(func) {
+  return func ? func(9) : 9;
+}
 
-console.log( seven(times(five())) ) ;
+function plus(y) {
+  return x => x + y;
+}
+function minus(y) {
+  return x => x - y;
+}
+function times(y) {
+  return x => x * y;
+}
+function dividedBy(y) {
+  return x => Math.trunc(x / y);
+}
+
+console.log( seven(times(five())) ) ; //must return 35
+console.log( four(plus(nine())) ); // must return 13
+console.log( eight(minus(three())) ); // must return 5
+console.log( six(dividedBy(two())) ); // must return 3
+console.log( eight(dividedBy(three())) );
