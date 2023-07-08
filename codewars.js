9fe6ec8d00b87ca47b6b5087e992ce3f7854b25d
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-07-08 17:29:31 +0300

    snail 1.0.2

--- codewars.js
+++ codewars.js
@@ -2,36 +2,36 @@
 
 let snail = function(array) {
   // enjoy
-  //let result = [toNum(array[0].join(','))];
-  let result = [toNum(array[0].join(','))];
+  /*
+  let result = [array[0].join(',')];
   result.push( array[1][array[1].length - 1] );
   result.push( array[2].reverse().join(',') );
   result.push( array[1].slice(0, 2).join(',') );
-  ;
-/*
-  for (let i = 1; i < array.length; i++) {
-    result.push(array[1]);
+  */
+  let result = [];
 
+  for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array[i].length; j++) {
       
-      console.log(result);
+      if (i === 0) {
+        result.push(array[0][j]);
+      }
+      if (i === array.length - 1) {
+        array[2].reverse();
+        result.push(array[2][j]);
+      }
+//      if (i !== 0 && i < array[array.length - 1] && j < array[i][array.length - 1].length) {
+//       result.push( array[array.length - 2][j] );
+//      }
     }
+
+    if (i === 1) {
+      result.push(array[1][array[1].length - 1]);
+    }
+
   }
-*/
+
   return result;
 }
 
-function toNum(str) {
-  for (let elem of str) {
-    elem = Number(elem);
-    console.log(elem);
-  }
-  return str;
-}
-
-function outArr(arr) {
-
-}
-
-//console.log(toNum())
 console.log(snail([[1,2,3], [4,5,6], [7,8,9]]));
