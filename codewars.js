d7e38d4c29147dd6544b00f5539f5cdfa20210dd
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-07-08 13:06:45 +0300

    snail 1.0.1

--- codewars.js
+++ codewars.js
@@ -1,22 +1,37 @@
 'use strict';
 
-  let result = [];
-
 let snail = function(array) {
   // enjoy
+  //let result = [toNum(array[0].join(','))];
+  let result = [toNum(array[0].join(','))];
+  result.push( array[1][array[1].length - 1] );
+  result.push( array[2].reverse().join(',') );
+  result.push( array[1].slice(0, 2).join(',') );
+  ;
+/*
+  for (let i = 1; i < array.length; i++) {
+    result.push(array[1]);
 
-  
-  for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < array[i]; i++) {
-      result.push(array[i][j]);
+    for (let j = 0; j < array[i].length; j++) {
+      
       console.log(result);
     }
   }
+*/
+  return result;
 }
 
+function toNum(str) {
+  for (let elem of str) {
+    elem = Number(elem);
+    console.log(elem);
+  }
+  return str;
+}
 
-let array = [[1,2,3], [4,5,6], [7,8,9]];
+function outArr(arr) {
 
+}
+
+//console.log(toNum())
 console.log(snail([[1,2,3], [4,5,6], [7,8,9]]));
-console.log(array[2][0]);
-console.log(result.push('3'));
