f126044f38a95b1875542ffffff3dde1a9757ba3
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-07-10 19:59:51 +0300

    snail 1.0.3 hardKostyl

--- codewars.js
+++ codewars.js
@@ -2,31 +2,31 @@
 
 let snail = function(array) {
   // enjoy
-  /*
-  let result = [array[0].join(',')];
-  result.push( array[1][array[1].length - 1] );
-  result.push( array[2].reverse().join(',') );
-  result.push( array[1].slice(0, 2).join(',') );
-  */
+
   let result = [];
 
   for (let i = 0; i < array.length; i++) {
+
+    if (i === 1) {
+      result.push(array[1][array[1].length - 1]);
+    }
+
     for (let j = 0; j < array[i].length; j++) {
       
       if (i === 0) {
         result.push(array[0][j]);
       }
-      if (i === array.length - 1) {
+      if (i ===  1) {
         array[2].reverse();
         result.push(array[2][j]);
       }
-//      if (i !== 0 && i < array[array.length - 1] && j < array[i][array.length - 1].length) {
-//       result.push( array[array.length - 2][j] );
-//      }
+
     }
 
     if (i === 1) {
-      result.push(array[1][array[1].length - 1]);
+
+      result = result.concat( array[i].slice(0, 2) );
+
     }
 
   }
