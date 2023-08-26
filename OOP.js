aca17f0191176e5d36f0d219d47a3b836532fcd4
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-08-26 14:09:46 +0300

    t8

--- OOP.js
+++ OOP.js
@@ -1,10 +1,32 @@
 class Employee {
+	constructor(name, position, department) {
+		this.name = name;
+		this.position = position;
+		this.department = department;
+	}
+}
+
+class Position {
 	constructor(name) {
 		this.name = name;
 	}
 }
 
-let emp1 = new Employee('john');
-let emp2 = new Employee('eric');
+class Department {
+	constructor(name) {
+		this.name = name;
+	}
+}
 
-console.log(emp1 === emp2);
+let posit = new Position('developer');
+let depar = new Department('development');
+
+let empl = new Employee('Jhon', posit, depar);
+
+console.log(posit.name);
+
+console.log(empl.name);
+console.log(empl);
+console.log(empl.position);
+console.log(empl.position.name);
+console.log(empl.department.name);
