88e7e27409483c2127b05d184a344bdfcad59a73
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-08-29 19:49:30 +0300

    t10

--- OOP.js
+++ OOP.js
@@ -1,64 +1,18 @@
-class EmployeesCollection {
-	#employees;
+class User {
+    constructor(name) {
+        this.name = name;
+    }
 
-	constructor() {
-		this.#employees = [];
-	}
-
-	addEmpl(employee) {
-		return this.#employees.push(employee);
-	}
-
-	showEmpl() {
-		for (let employee of this.#employees) {
-			console.log(employee.getShowName());
-		}
-	}
-
-	showAllSalary() {
-		let sum = 0;
-		for (let employee of this.#employees) {
-			sum += employee.getShowSalary();
-		}
-		return sum;
-	}
-
-	showAvrSalary() {
-		let sum = 0;
-		for (let employee of this.#employees) {
-			sum += employee.getShowSalary();
-		}
-		return sum/this.#employees.length;
-	}
+    show() {
+        return this.name;
+    }
 }
 
-class Employee {
-	#name;
-	#salary;
+class Employee extends User {
 
-	constructor(name, salary) {
-		this.#name = name;
-		this.#salary = salary;
-	}
-
-	getShowName() {
-		return this.#name;
-	}
-
-	getShowSalary() {
-		return this.#salary;
-	}
 }
 
-let employees = new EmployeesCollection;
+let empl = new Employee('Jhon');
 
-employees.addEmpl(new Employee('Jhon', 2000));
-employees.addEmpl(new Employee('Michael', 3000));
-employees.addEmpl(new Employee('Bob', 4000));
-employees.addEmpl(new Employee('Jesica', 5000));
-
-//console.log(employees);
-
-employees.showEmpl();
-console.log(employees.showAllSalary());
-console.log(employees.showAvrSalary());
+console.log(empl.show());
+console.dir(empl.show());
