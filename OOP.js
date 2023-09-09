ebb6dd0b2b10b38970c83911d8c4d501bbef7ee7
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-09-09 14:39:51 +0300

    t26

--- OOP.js
+++ OOP.js
@@ -1,26 +1,42 @@
-class User {
-    constructor(name, surn) {
-        this.name = name;
-        this.surn = surn;
+class Circle {
+    #radius;
+
+    constructor(x, y, radius, firstRadians, degrees) {
+        this.x = x;
+        this.y = y;
+        this.#radius = radius;
+        this.firstRadians = firstRadians;
+        this.degrees = degrees;
     }
 
-    get fullName() {
-        return `${this.name} ${this.surn}`;
+    canvas() {
+        let canv = document.querySelector('#canvas');
+        let ctx = canv.getContext('2d');
+
+        ctx.arc(this.x, this.y, this.#radius, this.firstRadians, this.radians(this.degrees));
+        ctx.stroke();
     }
 
-    set fullName(full) {
-        [this.name, this.surn] = full.split(' ');
+    radians(degrees) {
+        return (Math.PI / 180) * degrees;
+    }
+
+    square() {
+        return this.#radius ** 2 * Math.PI;
+    }
+
+    lengthCirc() {
+        return 2 * this.#radius * Math.PI;
     }
 }
 
-let user = new User('John', 'Smith');
+let circle1 = new Circle(150, 150, 75, 0, 360);
+circle1.canvas();
 
-console.log(user.name);
-console.log(user.surn);
-console.log(user.fullName);
+let circle2 = new Circle(150, 150, 100, 0, 360)
+circle2.canvas();
+console.log(circle2.lengthCirc());
 
-user.fullName = 'Michaell Westt';
-
-console.log(user.name);
-console.log(user.surn);
-console.log(user.fullName);
+let circle3 = new Circle(150, 150, 150, 0, 360)
+circle3.canvas();
+console.log(circle3.square());
