2cba72bdb0d1cd90601140585b87c74b4f6fb6cf
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-09-09 12:54:15 +0300

    t25

--- OOP.js
+++ OOP.js
@@ -7,9 +7,19 @@ class User {
     get fullName() {
         return `${this.name} ${this.surn}`;
     }
+
+    set fullName(full) {
+        [this.name, this.surn] = full.split(' ');
+    }
 }
 
 let user = new User('John', 'Smith');
+
+console.log(user.name);
+console.log(user.surn);
+console.log(user.fullName);
+
+user.fullName = 'Michaell Westt';
 
 console.log(user.name);
 console.log(user.surn);
