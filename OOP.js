e35eac21e4201bda9688d0196b2fc09b0b79e9c3
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-09-10 15:15:15 +0300

    t29

--- OOP.js
+++ OOP.js
@@ -1,59 +1,12 @@
-class Text {
-    constructor(text) {
-        this.text = text;
+class Period {
+    constructor(time1, time2) {
+        this.time1 = time1;
+        this.time2 = time2;
     }
 
-    countSimb() {
-        return this.text.length;
-    }
 
-    countLett() {
-        return this.text.match(/[A-Za-z]/g).length;
-    }
-
-    countGap() {
-        return this.text.match(/[ ]/g).length;
-    }
-
-    countDiffSimbGap() {
-        return this.countSimb() - this.countGap();
-    }
-
-    arrWord() {
-        let result = [];
-        let words = this.text.split(' ');
-        
-        for (let word of words) {
-            if ( word.match(/[A-Za-z]/g) ) {
-                result.push(word);
-            }
-        }
-
-        return result;
-    }
-
-    arrSentence() {
-        let result;
-
-        if ( this.text.match(/[.]/g) ) {
-            result = this.text.split('.');
-        }
-        
-        for (let res of result) {
-            if (res === '') {
-                result.pop();
-            }
-        }
-
-        return result;
-    }
 }
 
-let text = new Text('Hello Jaisy . It is me .');
+let period = new Period(new Date('2023-09-08'), new Date('2023-07-12'));
 
-console.log(text.countSimb());
-console.log(text.countLett());
-console.log(text.countGap());
-console.log(text.countDiffSimbGap());
-console.log(text.arrWord());
-console.log(text.arrSentence());
+console.log(period);
