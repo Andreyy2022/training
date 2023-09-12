ce72257c9109e15272774111a91db6f8746bf20d
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-09-12 22:02:32 +0300

    t32

--- OOP.js
+++ OOP.js
@@ -31,3 +31,22 @@ console.log(zate.date());
 console.log(zate.dateY());
 console.log(zate.dateMNum());
 console.log(zate.dateDayName());
+
+class ZateExt extends Zate {
+    
+    addYear(years) {
+        this.date().setFullYear(years + this.date().getFullYear());
+        console.log(years + this.date().getFullYear());
+        console.log(this.date());
+        return this.date();
+//        console.log(years + this.date().getFullYear());
+//        return this.date(years + this.date().getFullYear(), this.mm, this.dd);
+    }
+}
+
+let zateExt = new ZateExt(2023, 5, 20);
+
+console.log(zateExt.date());
+console.log(zateExt);
+zateExt.addYear(2);
+console.log(zateExt.date());
