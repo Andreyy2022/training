4dd89b7f0f3f887f528c9bd7fa2137427ee72dbf
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-09-12 20:22:06 +0300

    t30

--- OOP.js
+++ OOP.js
@@ -4,9 +4,21 @@ class Period {
         this.time2 = time2;
     }
 
+    showSec() {
+        return (this.time2 - this.time1) / 1000;
+    }
 
+    showMin() {
+        return (this.time2 - this.time1) / 1000 / 60;
+    }
+
+    showDays() {
+        return (this.time2 - this.time1) / 1000 / 60 / 60 / 24;
+    }
 }
 
-let period = new Period(new Date('2023-09-08'), new Date('2023-07-12'));
+let period = new Period(new Date('2023-06-08'), new Date('2023-07-12'));
 
-console.log(period);
+console.log(period.showSec());
+console.log(period.showMin());
+console.log(period.showDays());
