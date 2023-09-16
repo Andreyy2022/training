6092f820e9293751db8cfeb63c7ac0df2c18d976
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-09-16 12:27:59 +0300

    t35

--- OOP.js
+++ OOP.js
@@ -6,7 +6,8 @@ class Month {
     }
 
     date() {
-        return this.dateVar = new Date();
+        this.dateVar = new Date();
+        return this.dateVar.setMonth(this.monthNum);
     }
 
     showMonthNum() {
@@ -16,6 +17,15 @@ class Month {
     showMonthName() {
         return this.dateVar.toLocaleString('default', {month: 'long'});
     }
+
+    showLastDate() {
+        return new Date( this.dateVar.getFullYear(), this.dateVar.getMonth() + 1, 0 ).getDate();
+    }
+
+    showFirstDay() {
+        this.dateVar = new Date( this.dateVar.getFullYear(), this.dateVar.getMonth(), 1 );
+        return this.dateVar.getDay();
+    }
 }
 
 let month = new Month(5);
@@ -23,3 +33,5 @@ month.date();
 console.log(month.dateVar);
 console.log(month.showMonthNum());
 console.log(month.showMonthName());
+console.log(month.showLastDate());
+console.log(month.showFirstDay());
