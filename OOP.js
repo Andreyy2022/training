a0fa32cf0feed45c8065b2195e1d016e4a0f2c18
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-09-17 13:42:55 +0300

    t38

--- OOP.js
+++ OOP.js
@@ -1,15 +1,18 @@
 class Store {
+
     set(key, value) {
+        value = JSON.stringify(value);
         localStorage.setItem(key, value);
     }
 
     get(key) {
-        localStorage.getItem(key)
+        console.log( JSON.parse(localStorage.getItem(key)) );
     }
 }
 
-let store = new Store();
-store.set('key', {a: 1, b: 2, c: 3});
+let store = new Store;
 
-let res = store.get('key');
-console.log(res); // {a: 1, b: 2, c: 3}
+store.set('key', {a: 1, b: 2, c: 3});
+store.get('key');
+//let res = store.get('key');
+//console.log(store.get('key')); // {a: 1, b: 2, c: 3}
