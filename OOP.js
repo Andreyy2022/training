660eac62d8fd0af7c747752a00fe3fd038edd781
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-10-18 21:32:14 +0300

    t41

--- OOP.js
+++ OOP.js
@@ -1,22 +1,30 @@
-function func() {
-    let num = 10;
 
-    return function() {
-        console.log(num);
-        num--;
-    }
+
+function func() {
+    let num = 4;
+
+
+        return function() {
+            if(num >= 0) {
+                console.log(num);
+                num--;
+            } else {
+                console.log('end');
+            }
+        }
 }
 
-func();
-func();
-func();
-
-func()();
 
 let newFun1 = func();
 let newFun2 = func();
 
 newFun1();
 newFun1();
+newFun1();
+newFun1();
+newFun1();
+newFun1();
+newFun1();
 
-newFun2();
+
+//newFun2()();
