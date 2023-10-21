83e9c27e727ea3704c9877c8cbe181f7d2421580
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-10-21 13:51:36 +0300

    t47

--- OOP.js
+++ OOP.js
@@ -1,5 +1,11 @@
-let arr1 = [1, 2, 3];
-let arr2 = [4, 5, 6];
+function func(...nums) {
+    let sum = 0;
 
-let arr  = ['a', ...arr1, 'b', 'c', ...arr2];
-console.log(arr);
+    for (let num of nums) {
+        sum += num;
+    }
+
+    return sum / nums.length;
+}
+
+console.log(func(3, 4, 5, 8, 1, 5));
