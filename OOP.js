483a25b0e71b7501cbfb335126f02d11db14409c
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-10-25 19:57:54 +0300

    t52

--- OOP.js
+++ OOP.js
@@ -4,12 +4,10 @@ let elem = document.querySelector('#elem');
 elem.addEventListener('blur', parent);
 
 function parent() {
-	console.log(this); // выведет ссылку на наш инпут
+	console.log(this.value); // что выведет?
 	
 	function child() {
-		console.log(this); // выведет undefined
+		console.log(this); // что выведет?
 	}
 	child();
 }
-
-console.log(elem);
