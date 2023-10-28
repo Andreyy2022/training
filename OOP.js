09d28ff7f3c5506b089bc4e90b42aade7e004996
Andreyy2022 <104273434+Andreyy2022@users.noreply.github.com>
2023-10-28 13:20:58 +0300

    t54

--- OOP.js
+++ OOP.js
@@ -1,25 +1,3 @@
-"use strict";
-
-;(function() {
-	let elem = document.querySelector('#div1'); // первый див
-	
-	function func(num) {
-		return num * num; // возведем в квадрат
-	}
-	
-	elem.addEventListener('click', function() {
-		this.textContent = func(elem.textContent);
-	});
-})();
-
-;(function() {
-	let elem = document.querySelector('#div2'); // второй див
-	
-	function func(num) {
-		return num * num * num; // возведем в куб
-	}
-	
-	elem.addEventListener('click', function() {
-		this.textContent = func(elem.textContent);
-	});
-})();
+console.log( avg.avg1([1, 2, 3, 4, 5]) );
+console.log( avg.avg2([1, 2, 3, 4, 5]) );
+console.log( avg.avg3([1, 2, 3, 4, 5]) );
